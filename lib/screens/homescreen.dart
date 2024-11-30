95c886cec59b43933897225142a6deea75722dc3
tiesmed2201 <tiesmedemblik2201@gmail.com>
2024-11-30 23:00:49 +0100

    improved styling of the car list page and added searchbar to filter on car brand and name

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app_development/screens/rental_view.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    //TODO kijken of user een rental heeft
+    bool hasRental = false;
+    return hasRental ? const Center(
       child: Text('Home'),
-    );
+    ) : RentalView();
   }
 }
