0673a67e9618063cc418c625e411fd81cbde0af6
Thomas Kreder <mail@tkturbo.nl>
2025-01-06 01:34:02 +0100

    Show offline bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mobile_app_development/screens/AccountScreen.dart';
 import 'package:mobile_app_development/screens/ChangeAccountInfoScreen.dart';
@@ -112,11 +113,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _router,
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        scaffoldBackgroundColor: const Color(0xFF6F82F8),
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: OfflineBuilder(
+        connectivityBuilder: (
+          BuildContext context,
+          List<ConnectivityResult> connectivity,
+          Widget child,
+        ) {
+          final bool connected =
+              !connectivity.contains(ConnectivityResult.none);
+          return Stack(
+            fit: StackFit.expand,
+            children: [
+              MaterialApp.router(
+                routerConfig: _router,
+                theme: ThemeData(
+                  primarySwatch: Colors.indigo,
+                  scaffoldBackgroundColor: const Color(0xFF6F82F8),
+                ),
+              ),
+              Positioned(
+                top: MediaQuery.of(context).padding.top,
+                left: 0.0,
+                right: 0.0,
+                height: 24.0,
+                child: connected
+                    ? const SizedBox.shrink()
+                    : Container(
+                        color: const Color(0xFFEE4400),
+                        child: const Center(
+                          child: Text(
+                            'OFFLINE',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+              ),
+            ],
+          );
+        },
+        child: const SizedBox.shrink(),
       ),
     );
   }
