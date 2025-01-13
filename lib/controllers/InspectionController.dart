8d3c652dd0aa160f8d1e68344c01884327649c3c
tiesmed2201 <tiesmedemblik2201@gmail.com>
2025-01-13 21:17:17 +0100

    Car list can now be filtered and sorted

--- lib/controllers/InspectionController.dart
+++ lib/controllers/InspectionController.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:mobile_app_development/services/ApiService.dart';
 import '../DependencyInjection.dart';
@@ -10,6 +11,15 @@ class InspectionController {
   final ApiService apiService = DependencyInjection.getIt.get<ApiService>();
 
   Future<bool> addInspection(InspectionModel inspectionModel) async {
+    print(inspectionModel.code);
+    print(inspectionModel.completed);
+    print(inspectionModel.description);
+    print(inspectionModel.id);
+    print(inspectionModel.odometer);
+    print(inspectionModel.photo);
+    print(inspectionModel.photoContentType);
+    print(inspectionModel.result);
+
     final response = await apiService.addInspection(inspectionModel);
 
     return response.statusCode >= 200 && response.statusCode < 300;
